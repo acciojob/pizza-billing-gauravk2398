ecdfc902e9de09237a671fc9a7a21effbd79c738
Gaurav Kumar <109330154+akashsInterra@users.noreply.github.com>
2023-01-17 19:55:34 +0530

    first after code

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,9 +5,35 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    int toppingVeg=70;
+    int toppingNonVeg=120;
+    int paperBagP=20;
+    int cheeseP=80;
+    int vegPizzaBaseP = 300;
+    int nonVegBaseP = 400;
+
+    String cheeseBill = "Extra Cheese Added: ";
+    String toppingBill = "Extra Toppings Added: ";
+    String paperBagBill = "Paperbag Added: ";
+    String totalBill = "Total Price: ";
+    String pizzaBaseBill = "Base Price Of The Pizza: ";
+    boolean cFlag=false;
+    boolean tFlag=false;
+    boolean pFlag=false;
+
+
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg){
+             price=vegPizzaBaseP;
+             bill = pizzaBaseBill +vegPizzaBaseP + "\n";
+        }else{
+            price=nonVegBaseP;
+            bill = pizzaBaseBill +  nonVegBaseP +"\n";
+        }
         // your code goes here
     }
 
@@ -17,18 +43,55 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!cFlag){
+            price +=cheeseP;
+            cFlag=true;
+        }
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!tFlag){
+            if(isVeg){
+                price+=toppingVeg;
+
+            }else{
+                price+=toppingNonVeg;
+            }
+            tFlag=true;
+        }
+
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!pFlag){
+            price+=paperBagP;
+            pFlag=true;
+        }
+
     }
 
     public String getBill(){
         // your code goes here
+
+        if(cFlag){
+            bill+=cheeseBill+cheeseP+"\n";
+        }
+        if(tFlag){
+            if(isVeg){
+                bill+=toppingBill+toppingVeg+"\n";
+            }else{
+                bill+=toppingBill+toppingNonVeg+"\n";
+            }
+        }
+        if(pFlag){
+            bill+=paperBagBill+paperBagP+"\n";
+        }
+        bill+=totalBill+price;
+        this.bill=bill;
+
         return this.bill;
     }
 }
